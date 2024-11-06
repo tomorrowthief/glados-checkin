7e5c27c6fefb57d3cbceb0516486676c211fa886
thief <zx_Liu_uestc@163.com>
2024-11-06 16:41:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const glados = async () => {
     return [
       'Checkin OK',
       `${checkin.message}`,
-      `Left Days ${Number(status.data.leftDays)}`,
+      `Left Days ${JSON.stringify(status)}`,
     ]
   } catch (error) {
     return [
